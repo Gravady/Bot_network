44f97ea2e10e5abf9fe2dc0433df65d62c205a5c
Gravady <markar6@kfvelev.se>
2024-12-04 19:49:21 +0100

    Funny computer

---
+++ Bot_network/computer_oopsie.hpp
@@ -0,0 +1,10 @@
+//This file is just a fun thing that will take in a condition from another file	to pretty 
+//much ruin or cause as much damage as possible to the PC and OS
+#ifndef COMMAND_FILE
+#define COMMAND_FILE
+	#include "screen_look.hpp" //Will not actully be screen_look when program is finnished
+	//Will probably take in the command through a function
+	constexpr auto ReturnCommand() {
+		//Return command whatever it might be to init destruction
+	}
+#endif
